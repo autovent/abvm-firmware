bf2c6643d745ff9d15783e76122ac90c34c83302
Christopher Woodall <woodall@beta.team>
2020-04-03 16:17:39 -0400

    Add motor pins

--- include/pins.h
+++ include/pins.h
@@ -9,4 +9,10 @@ const uint8_t current_pin = PIN_A1;
 const uint8_t green_led_pin = 3;
 const uint8_t red_led_pin = 4;
 const uint8_t depth_in_pin = PIN_A2;
+
+const uint8_t motor_pwm_pin = 2;
+const uint8_t motor_dir_pin = 1;
+const uint8_t motor_enca_pin = 6;
+const uint8_t motor_encb_pin = 7;
+
 #endif // PINS_H
