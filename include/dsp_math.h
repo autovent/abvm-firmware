411f30e5c539849d3f72383bdd5586488db32528
Christopher Woodall <woodall@beta.team>
2020-04-06 15:53:04 -0400

    Add signof and float comparisons

--- include/dsp_math.h
+++ include/dsp_math.h
@@ -1,18 +1,35 @@
 #ifndef DSP_MATH_H_
 #define DSP_MATH_H_
 
-constexpr float saturate(float val, float min, float max)
-{
-    // clang-format off
+#include <math.h>
+constexpr float saturate(float val, float min, float max) {
+  // clang-format off
     return (val < min) ? min 
                        : ((val > max) ? max 
                                       : val);
-    // clang-format on
+  // clang-format on
 }
 
-constexpr float signof(float x)
+constexpr float signof(float x) { return (float)((x > 0) - (x < 0)); }
+
+bool approximatelyEqual(float a, float b, float epsilon)
 {
-    return (float)((x > 0) - (x < 0));
+    return fabs(a - b) <= ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+bool essentiallyEqual(float a, float b, float epsilon)
+{
+    return fabs(a - b) <= ( (fabs(a) > fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+bool definitelyGreaterThan(float a, float b, float epsilon)
+{
+    return (a - b) > ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+bool definitelyLessThan(float a, float b, float epsilon)
+{
+    return (b - a) > ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
 }
 
 #endif
