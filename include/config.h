ddccb7f15241aadf9b133332c1de002058dec1a2
Christopher Woodall <woodall@beta.team>
2020-04-06 15:52:39 -0400

    Tuned PID controller

--- include/config.h
+++ include/config.h
@@ -28,8 +28,8 @@ constexpr MotorParameters kRobotZone_638312_16RPM = {515.63, 48};
 
 #if defined(MOTOR_GOBILDA_30RPM)
 constexpr MotorParameters kMotorParams = kGoBilda_YellowJacket_5202_0002_0188_30RPM;
-constexpr PID::Params kMotorSpeedPidParams = {3, .3, 0};
-constexpr PID::Params kMotorPosPidParams = {10, .3, 0};
+constexpr PID::Params kMotorSpeedPidParams = {4, 0, .0};
+constexpr PID::Params kMotorPosPidParams = {13, .02, 0 }; //.02};
 #elif defined(MOTOR_ROBOTZONE_16RPM)
 constexpr MotorParameters kMotorParams = kRobotZone_638312_16RPM;
 constexpr PID::Params kMotorSpeedPidParams = {7, .5, 0};
@@ -46,16 +46,17 @@ constexpr uint32_t kMeasurementUpdatePeriod_ms = 10; // Match the 50 freq of the
 
 // Configuration parameters
 // ! EDIT theses as needed
+constexpr uint32_t kTimeToIdle_ms = 2000;
 constexpr int32_t kMinBPM = 8;
 constexpr int32_t kMaxBPM = 18;
 constexpr int32_t kSlowestBreathTime_ms = 1000*60/kMinBPM; 
 constexpr int32_t kFastestBreathTime_ms = 1000*60/kMaxBPM;
-constexpr float kIdlePositiong_deg = 20;
+constexpr float kIdlePositiong_deg = 0;
 constexpr float kOpenPosition_deg = 35; // Change this to a value where the servo is just barely compressing the bag
 constexpr float kMinClosedPosition_deg
     = 60; // Change this to a value where the servo has displaced the appropriate amount.
 constexpr float kMaxClosedPosition_deg
-    = 89.5; // Change this to a value where the servo has displaced the appropriate amount.
+    = 85; // Change this to a value where the servo has displaced the appropriate amount.
 
 // I : E Inspiration to Expiration Ratio
 constexpr IERatio kIERatio = {1, 2};
