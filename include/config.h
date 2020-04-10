e21828f9d7a93a922ad7a48fac2ac0a0656b465e
Christopher Woodall <woodall@beta.team>
2020-04-10 13:53:08 -0400

    Latest test setup

--- include/config.h
+++ include/config.h
@@ -28,8 +28,12 @@ constexpr MotorParameters kRobotZone_638312_16RPM = {515.63, 48};
 #if defined(MOTOR_GOBILDA_30RPM)
 constexpr MotorParameters kMotorParams =
     kGoBilda_YellowJacket_5202_0002_0188_30RPM;
-constexpr PID::Params kMotorVelPidParams = {4, 0, .0};
-constexpr PID::Params kMotorPosPidParams = {13, .02, 0};  //.02};
+constexpr PID::Params kMotorVelPidParams = {4, .0, .0};
+constexpr PID::Params kMotorPosPidParams = {14, .1, .02};  //.02};
+constexpr Range kMotorVelLimits = {-20 * 0.104719755,
+                                    20 * 0.104719755};  // RPM to rads/sec
+constexpr Range kMotorPosLimits = {0, (95 * 2 * PI) / 360};
+
 #elif defined(MOTOR_ROBOTZONE_16RPM)
 constexpr MotorParameters kMotorParams = kRobotZone_638312_16RPM;
 constexpr PID::Params kMotorVelPidParams = {6.5, .05, 0};
@@ -45,7 +49,7 @@ constexpr uint32_t kCurrentUpdatePeriod_ms = 2;
 constexpr uint32_t kPositionUpdate_ms =
     10;  // Match the 50 freq of the servo motors
 constexpr uint32_t kMeasurementUpdatePeriod_ms =
-    10;  // Match the 50 freq of the servo motors
+    10; 
 
 // Configuration parameters
 // ! EDIT theses as needed
@@ -54,7 +58,11 @@ constexpr int32_t kMinBPM = 8;
 constexpr int32_t kMaxBPM = 18;
 constexpr int32_t kSlowestBreathTime_ms = 1000 * 60 / kMinBPM;
 constexpr int32_t kFastestBreathTime_ms = 1000 * 60 / kMaxBPM;
+constexpr int32_t kPlateauTime_ms = 500;
+
+#ifdef CONFIG_SHORT_SPIRIT_FINGERS
 constexpr float kIdlePositiong_deg = 0;
+
 constexpr float kOpenPosition_deg =
     45;  // Change this to a value where the servo is just barely compressing
          // the bag
@@ -64,6 +72,20 @@ constexpr float kMinClosedPosition_deg =
 constexpr float kMaxClosedPosition_deg =
     88;  // Change this to a value where the servo has displaced the appropriate
          // amount.
+#else defined(CONFIG_LONG_SPIRIT_FINGERS)
+constexpr float kIdlePositiong_deg = 10;
+
+constexpr float kOpenPosition_deg =
+    35;  // Change this to a value where the servo is just barely compressing
+         // the bag
+constexpr float kMinClosedPosition_deg =
+    40;  // Change this to a value where the servo has displaced the appropriate
+         // amount.
+constexpr float kMaxClosedPosition_deg =
+    94;  // Change this to a value where the servo has displaced the appropriate
+         // amount.
+
+#endif
 
 // I : E Inspiration to Expiration Ratio
 constexpr IERatio kIERatio = {1, 2};
