12e771e79495708a29410ecd336dbe35238ea638
Christopher Woodall <woodall@beta.team>
2020-04-12 18:56:10 -0400

    Basic ventilator functionality and motion patterns

--- Inc/math/conversions.h
+++ Inc/math/conversions.h
@@ -3,3 +3,4 @@
 
 constexpr float deg_to_rad(float x) { return x * 2 * M_PI / 360.0f; }
 constexpr float rad_to_deg(float x) { return 360.0f * x / (2 * M_PI); }
+constexpr uint32_t bpm_to_time_ms(float bpm) { return 1000 * 60 / bpm; }
