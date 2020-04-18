bf060e77a2282f432c6c50e682170aaf98a0ad1a
Christopher Woodall <woodall@beta.team>
2020-04-18 15:00:10 -0400

    Add map function

--- Inc/math/dsp.h
+++ Inc/math/dsp.h
@@ -22,4 +22,10 @@ inline float dt_rate_limit(float target, float current, float rate) {
     float dx = target - current;
     return (fabsf(dx) > rate) ? (current + signof(dx) * rate) : target;
 }
+
+template <typename T>
+constexpr T map(T x, T in_min, T in_max, T out_min, T out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 #endif
