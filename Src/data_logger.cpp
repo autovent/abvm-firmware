3b0de8d9721b5bce138c6ce5cb13dd5905ea8df2
Christopher Woodall <woodall@beta.team>
2020-05-03 18:33:40 -0400

    Actuakky retyrb tge cirrect vakye

--- Src/data_logger.cpp
+++ Src/data_logger.cpp
@@ -1,14 +1,15 @@
 #include "data_logger.h"
+
 #include "clock.h"
 #include "math/conversions.h"
 
-DataLogger::DataLogger(uint8_t id, ADS1231 *pressure_sensor, Servo *motor,
-    DRV8873 *motor_driver, VentilatorController *vent) :
-        CommEndpoint(id, &log, sizeof(dataLog), true),
-        pressure_sensor(pressure_sensor),
-        motor(motor),
-        motor_driver(motor_driver),
-        vent(vent) {}
+DataLogger::DataLogger(uint8_t id, ADS1231 *pressure_sensor, Servo *motor, DRV8873 *motor_driver,
+                       VentilatorController *vent)
+    : CommEndpoint(id, &log, sizeof(dataLog), true),
+      pressure_sensor(pressure_sensor),
+      motor(motor),
+      motor_driver(motor_driver),
+      vent(vent) {}
 
 uint8_t DataLogger::read(void *data, size_t size) {
     log.time = msec_to_sec(millis());
@@ -22,5 +23,5 @@ uint8_t DataLogger::read(void *data, size_t size) {
     log.vent_closed_pos = vent->get_closed_pos();
     log.vent_open_pos = vent->get_open_pos();
 
-    CommEndpoint::read(data, size);
+    return CommEndpoint::read(data, size);
 }
