78c259d84783ba5c99600653c962848c7e2a88dd
Chris Townsend <ctownsendvt@gmail.com>
2020-05-18 09:58:04 -0400

    adjusted time fudge factor

--- Inc/math/conversions.h
+++ Inc/math/conversions.h
@@ -9,7 +9,7 @@ constexpr float rad_to_deg(float x) {
     return 360.0f * x / (2 * M_PI);
 }
 constexpr uint32_t bpm_to_time_ms(float bpm) {
-    return 1000 * 60 / bpm;
+    return 900 * 60 / bpm; //  1000 * 60 / bpm;
 }
 constexpr float psi_to_cmH2O(float x) {
     return 70.307 * x;
